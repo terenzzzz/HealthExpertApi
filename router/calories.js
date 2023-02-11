af155ef9bf3f36564cb366eb84ba917a175e1054
terenzzzz <terence592592@gmail.com>
2023-02-11 16:28:06 +0000

    Change caloriesInfo to calories

--- router/calories.js
+++ router/calories.js
@@ -6,6 +6,8 @@ const router = express.Router()
 //导入处理函数
 const caloriesHandler = require('../router_handler/calories_handler')
 
+router.get('/calories', caloriesHandler.calories)
+
 router.get('/caloriesInfo', caloriesHandler.caloriesInfo)
 
 router.post('/addCalories', caloriesHandler.addCalories)
--- router_handler/calories_handler.js
+++ router_handler/calories_handler.js
@@ -2,6 +2,18 @@
 const db = require('../db/index')
 
 // 获取用户卡路里模块数据
+exports.calories = (req, res) => {
+    const sqlQuery = `select * from Calories where idUser=?`
+    db.query(sqlQuery, req.user.idUser, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.length > 0) {
+            res.send({ status: 200, message: '获取用户卡路里信息成功！', data: results})
+        } else {
+            return res.cc('获取用户卡路里信息失败！')
+        }
+    })
+}
+
 exports.caloriesInfo = (req, res) => {
     const sqlQuery = `select * from Calories where idUser=?`
     db.query(sqlQuery, req.user.idUser, (err, results) => {
