01004916c37f8171dbbe05e465a03ee6bad873fa
terenzzzz <terence592592@gmail.com>
2023-02-11 17:37:48 +0000

    1

--- router_handler/calories_handler.js
+++ router_handler/calories_handler.js
@@ -7,9 +7,9 @@ exports.calories = (req, res) => {
     db.query(sqlQuery, req.user.idUser, (err, results) => {
         if (err) return res.cc(err)
         if (results.length > 0) {
-            res.send({ status: 200, message: '获取用户卡路里信息成功！', data: results})
+            res.send({ status: 200, message: '获取用户卡路里信息成功1！', data: results})
         } else {
-            return res.cc('获取用户卡路里信息失败！')
+            return res.cc('获取用户卡路里信息失败1！')
         }
     })
 }
