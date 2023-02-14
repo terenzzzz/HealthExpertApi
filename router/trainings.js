db973d1fa3963745c6093e8428d7dc08b45f1afa
terenzzzz <terence592592@gmail.com>
2023-02-14 22:20:41 +0000

    addLocationAPI

--- router/trainings.js
+++ router/trainings.js
@@ -6,7 +6,9 @@ const router = express.Router()
 //导入处理函数
 const trainingsHandler = require('../router_handler/trainings_handler')
 
-router.get("/trainings",trainingsHandler.trainings)
+router.get("/trainings", trainingsHandler.trainings)
+
+router.post("/addLocations",trainingsHandler.addLocations)
 
 //共享
 module.exports = router
--- router_handler/trainings_handler.js
+++ router_handler/trainings_handler.js
@@ -13,3 +13,34 @@ exports.trainings = (req, res) => {
         }
     })
 }
+
+exports.addLocations = (req, res) => { 
+    // var locations = [
+    //     { latitude: 53.3805467, longitude: -1.4769983 },
+    //     { latitude: 53.3805467, longitude: -1.4769983 },
+    //     { latitude: 53.3805467, longitude: -1.4769983 },
+    // ];
+    // var str = JSON.stringify(locations);
+    // var data2 = JSON.parse(str);
+    // console.log(str);
+    // console.log(data2);
+
+    const sqlInsert = `INSERT INTO TrainingLocations (idTraining,Latitude,Longitude) VALUES ?`
+    const locations = JSON.parse(req.body.locations)
+    // console.log(locations);
+    const data = []
+    locations.forEach((v,i) => {
+        data.push([1,v.latitude,v.longitude])
+    });
+
+    db.query(sqlInsert, [data], (err, results) => { 
+        if (err) { return res.cc(err) }
+        if (results.affectedRows >= 1) { 
+            return res.send({
+                status: 0,
+                message: '定位添加成功'
+            })
+        }
+        return res.cc('定位添加失败')
+    })
+}
