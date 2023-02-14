8e2d90e252502f0504343020dac5f06c35a691e1
terenzzzz <terence592592@gmail.com>
2023-02-14 17:10:29 +0000

    newInfo API

--- router/news.js
+++ router/news.js
@@ -6,7 +6,9 @@ const router = express.Router()
 //导入处理函数
 const newsHandler = require('../router_handler/news_handler')
 
-router.get('/news',newsHandler.news)
+router.get('/news', newsHandler.news)
+
+router.get('/newInfo', newsHandler.newsInfo)
 
 //共享
 module.exports = router
--- router_handler/news_handler.js
+++ router_handler/news_handler.js
@@ -12,3 +12,16 @@ exports.news = (req, res) => {
         }
     })
 }
+
+// 获取卡路里详细信息
+exports.newsInfo = (req, res) => {
+    const sqlQuery = `select * from News where id=?`
+    db.query(sqlQuery, req.query.id, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.length == 1) {
+            res.send({ status: 200, message: '获取新闻详细信息成功！', data: results[0]})
+        } else {
+            return res.cc('获取新闻详细信息失败！')
+        }
+    })
+}
