0167f8212f484fa48c5815c00ba5d6e2c5603cf8
terenzzzz <terence592592@gmail.com>
2023-02-15 20:53:29 +0000

    addwater API

--- router/water.js
+++ router/water.js
@@ -8,5 +8,7 @@ const waterHandler = require('../router_handler/water_handler')
 
 router.get("/waters", waterHandler.waters)
 
+router.post("/addWaters", waterHandler.addWaters)
+
 // 向外共享路由对象 
 module.exports = router
--- router_handler/water_handler.js
+++ router_handler/water_handler.js
@@ -1,7 +1,7 @@
 // 导入数据库操作模块
 const db = require('../db/index')
 
-// 获取用户卡路里模块数据
+// 获取用户喝水模块数据
 exports.waters = (req, res) => {
     const sqlQuery = `select * from Waters where idUser=?`
     db.query(sqlQuery, req.user.idUser, (err, results) => {
@@ -10,6 +10,29 @@ exports.waters = (req, res) => {
             res.send({ status: 200, message: '获取用户喝水信息成功！', data: results})
         } else {
             return res.cc('获取用户喝水信息失败！')
+        }
+    })
+}
+
+// 添加喝水数据
+exports.addWaters = (req, res) => {
+    let time = req.body.time
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth()+1;
+    let date = today.getDate();
+    let dateTime = year + "-" + (month + 1) + "-" + date + " " + time + ":00"// 2023-3-11 08：00
+    console.log(dateTime);
+    const sqlInsert = `insert into Waters set ?`
+    db.query(sqlInsert, {
+        idUser: req.user.idUser, Type: req.body.type, Title: req.body.title,
+        Content: req.body.content, Value: req.body.value, Time: dateTime
+    }, function (err, results) {
+        if (err) return res.cc(err)
+        if (results.affectedRows == 1) {
+            res.send({ status: 200, message: '添加喝水数据成功！'})
+        } else {
+            return res.cc('添加喝水数据失败！')
         }
     })
 }
