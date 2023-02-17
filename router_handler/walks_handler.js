84585ee3af85512d8db5183862d64a74c5830ec5
terenzzzz <terence592592@gmail.com>
2023-02-17 14:47:13 +0000

    update add walk

--- router_handler/walks_handler.js
+++ router_handler/walks_handler.js
@@ -1,9 +1,10 @@
 // 导入数据库操作模块
 const db = require('../db/index')
+const today = require('../utils/today');
 
 // 获取用户当天行走信息数据
 exports.walks = (req, res) => {
-    const sqlQuery = `select * from Walks where idUser=? and Date(Date)="${toDate()}"`
+    const sqlQuery = `select * from Walks where idUser=? and Date(Date)="${today.toDate()}"`
     db.query(sqlQuery, req.user.idUser, (err, results) => {
         if (err) return res.cc(err)
         if (results.length == 1) {
@@ -15,7 +16,7 @@ exports.walks = (req, res) => {
 }
 
 exports.walkSteps = (req, res) => {
-    const sqlQuery = `select * from WalkSteps where Date(Time)="${toDate()}"`
+    const sqlQuery = `select * from WalkSteps where Date(Time)="${today.toDate()}"`
     db.query(sqlQuery,  (err, results) => {
         if (err) return res.cc(err)
         if (results.length > 0) {
@@ -28,7 +29,7 @@ exports.walkSteps = (req, res) => {
 
 // 添加行走数据
 exports.addWalk = (req, res) => {
-    const sqlQuery = `select * from Walks where Date="${toDate()}"`
+    const sqlQuery = `select * from Walks where Date="${today.toDate()}"`
     db.query(sqlQuery, req.query.idWalk, (err, results) => { 
         console.log(results.length);
         if (results.length == 0) {
@@ -39,7 +40,7 @@ exports.addWalk = (req, res) => {
             const sqlInsert = `insert into Walks set ?`
             db.query(sqlInsert, {
                 idUser: req.user.idUser, TotalSteps: step, Calories: calories,
-                Distance: distance, Date: toDateTime()
+                Distance: distance, Date: today.toDateTime()
             }, function (err, res1) {
                 if (err) return res.cc(err)
                 if (res1.affectedRows == 1) {
@@ -55,7 +56,7 @@ exports.addWalk = (req, res) => {
             let calories = parseInt(results[0].Calories) + newStep * 0.04
             let distance = parseFloat(results[0].Distance) + newStep * 0.0005
             const sqlUpdate = `update Walks set TotalSteps = ?,
-                Calories = ?,Distance = ?,Date = ${toDateTime()} where Date="${toDate()}"`
+                Calories = ?,Distance = ?,Date = "${today.toDateTime()}" where Date="${today.toDate()}"`
             db.query(sqlUpdate, [step, calories, distance], (err, res2) => {
                 if(err) return res.cc(err.message)
                 if (res2.affectedRows===1){
@@ -74,7 +75,7 @@ exports.addWalk = (req, res) => {
 // 添加步数数据
 exports.addWalkSteps = (req, res) => {
     const sqlInsert = `insert into WalkSteps set ?`
-    db.query(sqlInsert, {Steps: req.body.steps,Time: toDateTime()}, function (err, results) {
+    db.query(sqlInsert, {Steps: req.body.steps,Time: today.toDateTime()}, function (err, results) {
         if (err) return res.cc(err)
         if (results.affectedRows == 1) {
             res.send({ status: 200, message: '添加步数数据成功！'})
@@ -84,25 +85,3 @@ exports.addWalkSteps = (req, res) => {
     })
 }
 
-function toDate() {
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1; // getMonth() returns zero-based index, so we need to add 1
-    let day = date.getDate();
-    let mysqlDate = year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
-    return mysqlDate
-}
-
-function toDateTime() {
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1; // getMonth() returns zero-based index, so we need to add 1
-    let day = date.getDate();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let mysqlDateTime = year + "-" + (month < 10 ? "0" + month : month) +
-        "-" + (day < 10 ? "0" + day : day) + " " + (hours < 10 ? "0" + hours : hours)
-        + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
-    return mysqlDateTime
-}
