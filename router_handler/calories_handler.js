ee0c9911733241d3cb6f0919a1dbe6e60b7fa7f5
terenzzzz <terence592592@gmail.com>
2023-02-17 15:45:27 +0000

    update add walk

--- router_handler/calories_handler.js
+++ router_handler/calories_handler.js
@@ -1,9 +1,10 @@
 // 导入数据库操作模块
 const db = require('../db/index')
+const today = require('../utils/today');
 
 // 获取用户卡路里模块数据
 exports.calories = (req, res) => {
-    const sqlQuery = `select * from Calories where idUser=?`
+    const sqlQuery = `select * from Calories where idUser=? Date(Time)="${today.toDate}"`
     db.query(sqlQuery, req.user.idUser, (err, results) => {
         if (err) return res.cc(err)
         if (results.length > 0) {
