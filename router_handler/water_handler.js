0162313cb338f52116d34e598875dba6b4095156
terenzzzz <terence592592@gmail.com>
2023-02-17 15:55:41 +0000

    update waters

--- router_handler/water_handler.js
+++ router_handler/water_handler.js
@@ -29,17 +29,10 @@ exports.watersInfo = (req, res) => {
 
 // 添加喝水数据
 exports.addWaters = (req, res) => {
-    let time = req.body.time
-    let today = new Date();
-    let year = today.getFullYear();
-    let month = today.getMonth()+1;
-    let date = today.getDate();
-    let dateTime = year + "-" + (month + 1) + "-" + date + " " + time + ":00"// 2023-3-11 08：00
-    console.log(dateTime);
     const sqlInsert = `insert into Waters set ?`
     db.query(sqlInsert, {
         idUser: req.user.idUser, Type: req.body.type, Title: req.body.title,
-        Content: req.body.content, Value: req.body.value, Time: dateTime
+        Content: req.body.content, Value: req.body.value, Time: today.toDateTime()
     }, function (err, results) {
         if (err) return res.cc(err)
         if (results.affectedRows == 1) {
