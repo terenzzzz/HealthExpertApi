2ff8644c4c0799403c2ed488c0876e9524a3b702
Zhicong Jiang <terence592592@gmail.com>
2023-04-27 08:50:58 +0100

    apidoc

--- router/heartRate.js
+++ router/heartRate.js
@@ -6,9 +6,26 @@ const router = express.Router()
 //导入处理函数
 const heartRateHandler = require('../router_handler/heartRate_handler')
 
-
+/**
+ * @api {get} /my/heartRates getHeartRates
+ * @apiGroup Heart Rate
+ *
+ * @apiHeader {String} Authorization Users Login AWT Token.
+ *  
+ * @apiQuery {String} date
+ *
+*/
 router.get('/heartRates', heartRateHandler.heartRates)
 
+/**
+ * @api {post} /my/addHeartRate AddHeartRate
+ * @apiGroup Heart Rate
+ *
+ * @apiHeader {String} Authorization Users Login AWT Token.
+ *  
+ * @apiBody {String} heartRate Heart Rate
+ *
+*/
 router.post('/addHeartRate', heartRateHandler.addHeartRate)
 
 //共享
